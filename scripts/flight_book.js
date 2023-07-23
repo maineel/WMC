9767c952acffea0f41f36bb5876f7ccd535b8a52
neel <neelxdxd@gmail.com>
2023-07-23 19:47:25 +0530

    Flight details page

--- scripts/flight_book.js
+++ scripts/flight_book.js
@@ -18,13 +18,33 @@ selectClass.addEventListener('change', function handleChange(event) {
     var classSelect = event.target.value;
     alert("Class: " + classSelect);
 });
-// const selectDate = document.getElementById('datepicker');
-// selectDate.addEventListener('change', function handleChange(event) {
-//     var dateSelect = event.target.value;
-//     alert("Date: " + dateSelect);
-// });
-const selectPassengers = document.getElementById('passengers');
-selectPassengers.addEventListener('change', function handleChange(event) {
-    var passengerSelect = event.target.value;
+const selectDate = document.getElementById('datepicker');
+selectDate.addEventListener('change', function handleChange(event) {
+    var dateSelect = event.target.value;
+    alert("Date: " + dateSelect);
+});
+const selectAdults = document.getElementById('adults');
+selectAdults.addEventListener('change', function handleChange(event) {
+    var adultSelect = event.target.value;
     alert("Passengers: " + passengerSelect);
+});
+const selectChildren = document.getElementById('children');
+selectChildren.addEventListener('change', function handleChange(event) {
+    var childrenSelect = event.target.value;
+    alert("Passengers: " + childrenSelect);
+});
+const selectType = document.getElementById('flight-type');
+selectType.addEventListener('change', function handleChange(event) {
+    var typeSelect = event.target.value;
+    alert("Passengers: " + typeSelect);
+});
+const selectCurrency = document.getElementById('currency');
+selectCurrency.addEventListener('change', function handleChange(event) {
+    var currencySelect = event.target.value;
+    alert("Passengers: " + currencySelect);
+});
+const selectReturnDate = document.getElementsByClassName('return-date')[0];
+selectReturnDate.addEventListener('change', function handleChange(event) {
+    var returnDateSelect = event.target.value;
+    alert("Date: " + returnDateSelect);
 });
