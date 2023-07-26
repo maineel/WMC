481961206ff730ef24a37d446f80d4bce127b10e
neel <neelxdxd@gmail.com>
2023-07-26 22:35:36 +0530

    updates to flight_details & ticket_details page

---
+++ scripts/ticket_details.js
@@ -0,0 +1,15 @@
+const selectName = document.getElementById('full-name');
+selectName.addEventListener('change', function handleChange(event) {
+    var namselect = event.target.value;
+    alert("Name: " + namselect);
+});
+const phonenumber = document.getElementById('number');
+phonenumber.addEventListener('change', function handleChange(event) {
+    var numberselect = event.target.value;
+    alert("Number: " + numberselect);
+});
+const emailSelect = document.getElementById('email');
+emailSelect.addEventListener('change', function handleChange(event) {
+    var selectEmail = event.target.value;
+    alert("Email: " + selectEmail);
+});
